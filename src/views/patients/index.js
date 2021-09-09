416c31995e7414cb388f0f3a7c943e71ea436e76
mozgbrasil <mozgbrasil@gmail.com>
2021-09-09 16:09:35 +0000

    test trigger component parent child

--- src/views/patients/index.js
+++ src/views/patients/index.js
@@ -3,8 +3,31 @@
 // #
 // #
 
-export function About(props) {
-    const { kind, ...other } = props;
+// We need to wrap component in `forwardRef` in order to gain
+// access to the ref object that is assigned using the `ref` prop.
+// This ref is passed as the second parameter to the function component.
+const About = forwardRef((props, ref) => {
+    const [state, setState] = useState(0);
+
+    // The component instance will be extended
+    // with whatever you return from the callback passed
+    // as the second argument
+    useImperativeHandle(ref, () => ({
+        Hello
+    }));
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {}, []);
+
+    const Hello = () => {
+        console.log('About->Hello');
+    };
+
+    const trigger_Parent = () => {
+        console.log('About->trigger_Parent');
+        // childRef_AntdSchemaTable.current.getPacients();
+    };
 
     return (
         <>
@@ -13,10 +36,11 @@ export function About(props) {
                     https://github.com/mozgbrasil/node-labs/blob/develop/views/tests/findup/scope/README_findup.md#findup
                 </a>
             </Typography>
+            <button onClick={() => trigger_Parent()}>trigger "Hello()" from Child 2 Parent</button>
             {/* <SimpleSnackbar /> */}
         </>
     );
-}
+});
 
 // #
 // #
@@ -908,51 +932,6 @@ export function AntdSchemaForm(props) {
 // #
 // #
 
-// We need to wrap component in `forwardRef` in order to gain
-// access to the ref object that is assigned using the `ref` prop.
-// This ref is passed as the second parameter to the function component.
-const Child = forwardRef((props, ref) => {
-    const [state, setState] = useState(0);
-
-    const getAlert = () => {
-        alert('getAlert from Child');
-        setState((state) => state + 1);
-    };
-
-    // The component instance will be extended
-    // with whatever you return from the callback passed
-    // as the second argument
-    useImperativeHandle(ref, () => ({
-        getAlert
-    }));
-
-    return (
-        <>
-            <h1>Count {state}</h1>
-            <button onClick={() => getAlert()}>Click Child</button>
-            <br />
-        </>
-    );
-});
-
-export const Parent = () => {
-    // In order to gain access to the child component instance,
-    // you need to assign it to a `ref`, so we call `useRef()` to get one
-    const childRef = useRef();
-
-    return (
-        <div>
-            <Child ref={childRef} />
-            <button onClick={() => childRef.current.getAlert()}>Click Parent</button>
-        </div>
-    );
-};
-
-// #
-// #
-// #
-// #
-
 import React, { useRef, useState, useEffect, forwardRef, useImperativeHandle } from 'react';
 
 // material-ui
@@ -1007,24 +986,34 @@ const App = () => {
         }
     }, []);
 
+    const Hello = () => {
+        console.log('App->Hello');
+    };
+
     // In order to gain access to the child component instance,
     // you need to assign it to a `ref`, so we call `useRef()` to get one
-    const childRef = useRef();
+    const childRef_About = useRef();
+    const childRef_AntdSchemaTable = useRef();
 
-    const getPacients = () => {
-        console.log('App->useRef->getPacient');
-        childRef.current.getPacients();
+    const trigger_About = () => {
+        console.log('App->trigger_About');
+        childRef_About.current.Hello();
+    };
+
+    const trigger_AntdSchemaTable = () => {
+        console.log('App->useRef->trigger_AntdSchemaTable');
+        childRef_AntdSchemaTable.current.getPacients();
     };
 
     return (
         <>
             <MainCard title="Escopo">
-                <About />
-                <Parent />
+                <About ref={childRef_About} />
+                <button onClick={() => trigger_About()}>trigger "Hello()" from Parent 2 Child</button>
             </MainCard>
             <MainCard title="Grade de Dados">
-                <AntdSchemaTable ref={childRef} />
-                <button onClick={() => getPacients()}>Atualizar</button>
+                <AntdSchemaTable ref={childRef_AntdSchemaTable} />
+                <button onClick={() => trigger_AntdSchemaTable()}>Atualizar</button>
             </MainCard>
             <MainCard title="Formulário">{data && <AntdSchemaForm value={data} />}</MainCard>
         </>
